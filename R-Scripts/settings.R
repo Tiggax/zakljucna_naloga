c5c4819efd0db5702ba00c4be50417916bb95494
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2024-07-31 00:33:30 +0200

    fix typo

--- R-Scripts/settings.R
+++ R-Scripts/settings.R
@@ -5,4 +5,4 @@ library(readr)
 library(scales)
 
 vals = c("vcd", "glucose", "glutamin", "oxygen","DO", "c_O2", "volume", "product")
-labels = c("VCD\n[MVC/mL min]", "glucose\n[g/L]", "glutamine\n[g/L]", "PID\n[L/min]","DO\n[%]", "c_O2\n[mol/L]", "volume\n[L]", "product\n[g/L]")
+labels = c("VCD\n[MVC/mL]", "glucose\n[g/L]", "glutamine\n[g/L]", "PID\n[L/min]","DO\n[%]", "c_O2\n[mol/L]", "volume\n[L]", "product\n[g/L]")
